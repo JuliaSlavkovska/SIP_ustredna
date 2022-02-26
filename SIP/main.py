06ffaba69962ad381c998b1893452fe4713f032d
JuliaSlavkovska <55793090+JuliaSlavkovska@users.noreply.github.com>
2022-02-26 20:39:29 +0100

    zaklad + hlaska registracie

--- SIP/main.py
+++ SIP/main.py
@@ -4,13 +4,23 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import sipfullproxy
+import socketserver
+import re
+import string
+import socket
+# import threading
+import sys
+import logging
+import time
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    sipfullproxy.ipaddress = "147.175.186.16"
+    print(sipfullproxy.ipaddress)
+    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (sipfullproxy.ipaddress, sipfullproxy.PORT)
+    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (sipfullproxy.ipaddress, sipfullproxy.PORT)
+    server = socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
+    server.serve_forever()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
